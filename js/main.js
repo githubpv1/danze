dc73d5a3febe3002612a43367207d26c9ce9759a
githubpv1 <pvgit20@gmail.com>
2020-08-21 22:07:53 +0300

    sd

--- js/main.js
+++ js/main.js
@@ -1,128 +1,149 @@
-
-		
-		/* carousel */
-	$('.carousel__list').slick({
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-			prevArrow: $('.prev3'),  
-			nextArrow: $('.next3'),
-			responsive: [
-    {
-      breakpoint: 992, //и меньше
-      settings: {
-        slidesToShow: 3
-             }
-			},
-		{
-      breakpoint: 768, 
-      settings: {
-        slidesToShow: 2
-             }
-			},	
-		{				
-			breakpoint: 576, 
-      settings: {
-        slidesToShow: 1
-             }			 
-    }] 
-    });
+objectFitImages(); //IE polyfill
 
 
-		/* carousel */ 
-	$('.slider').slick({
-		infinite: true,
-    rows: 2,
-		//slidesPerRow: 2,
-		slidesToScroll: 1,
-		slidesToShow: 2,
-		prevArrow: $('.prev'), 
-		nextArrow: $('.next'),
-		responsive: [
-	{
-		breakpoint: 768, //и меньше
-		settings: {
-		//	slidesToShow: 1,
-		//	slidesToScroll: 1
-					 }
-    }] 
-    });
-		
-		/* carousel */
-	var mySwiper1 = new Swiper ('.reviews_list', {
-    slidesPerView: 2,
-		spaceBetween: 20,
-		slidesPerColumn: 2,
-    loop: true,
-    
-    navigation: {
-      nextEl: '.next_reviews',
-      prevEl: '.prev_reviews',
-    },
-		breakpoints: {
-			580: {
-				slidesPerView: 1
-			},
-		}
-  })
-		
-		
-		/* video-carousel */
-	var mySwiper = new Swiper ('.s1', {
-    slidesPerView: 2,
-		spaceBetween: 20,
-		loop: true,
-		grabCursor: true,
-    navigation: {
-      nextEl: '.next_video',
-      prevEl: '.prev_video',
-    },
-		pagination: {
-			el: '.swiper-pagination1',
-			clickable: true,
+document.querySelector('.nav__burger').onclick = function () {
+	this.classList.toggle('active');
+	document.querySelector('.nav').classList.toggle('active');
+	document.querySelector('body').classList.toggle('lock');
+}
+
+
+var mySwiper = new Swiper('.news__swiper', {
+	// spaceBetween: 20,
+	loop: true,
+	autoHeight: true,
+	grabCursor: true,
+	// effect: 'fade',
+	// fadeEffect: { //без этого наслаивается html контент
+	// 	crossFade: true
+	// },
+	navigation: {
+		nextEl: '.news__btn_next',
+		prevEl: '.news__btn_prev',
+	},
+	pagination: {
+		el: '.news__swiper_pagination',
+		clickable: true,
+	},
+	breakpointsInverse: true,
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 40
 		},
-		breakpoints: {
-			580: {
-				slidesPerView: 1
-			},
+		1200: {
+			slidesPerView: 4,
+			spaceBetween: 80
 		}
-  });
- 
+	}
+});
 
- 
-	/* carousel */
-	$('.reviews__list').slick({
-      infinite: true,
-      slidesToShow: 2,
-      slidesToScroll: 1,
-			rows: 2,
-			prevArrow: $('.prev_carousel'),  
-			nextArrow: $('.next_carousel'),
-			responsive: [
-		{				
-			breakpoint: 580, 
-      settings: {
-        slidesToShow: 1,
-				rows: 1
-             }			 
-    }] 
-    });
- 
- 
- 
-	objectFitImages(); //IE polyfill
-	
-	/*reviews jQery
-	
-	$('[data-img]').click(function(){
+
+
+/* store-gallery */
+if (window.innerWidth < 768) {
+	var mySwiper = new Swiper('.store__swiper', {
+		//spaceBetween: 20,
+		// loop: true,
+		grabCursor: true,
+		navigation: {
+			nextEl: '.store__btn_next',
+			prevEl: '.store__btn_prev',
+		},
+		pagination: {
+			el: '.store__swiper_pagination',
+			clickable: true,
+		}
+	});
+}
+
+
+/* video-carousel */
+var mySwiper = new Swiper('.video__swiper', {
+	slidesPerView: 1,
+	loop: true,
+	grabCursor: true,
+	navigation: {
+		nextEl: '.video__btn_next',
+		prevEl: '.video__btn_prev',
+	},
+	pagination: {
+		el: '.video__swiper_pagination',
+		clickable: true,
+	},
+	breakpoints: {
+		1200: {
+			spaceBetween: 20,
+			slidesPerView: 2
+		},
+	}
+});
+
+
+
+/* carousel reviews */
+var mySwiper_reviews = new Swiper('.reviews__swiper', {
+	loop: true,
+	grabCursor: true,
+	navigation: {
+		nextEl: '.rewiews__btn_next',
+		prevEl: '.rewiews__btn_prev',
+	},
+	breakpoints: {
+		768: {
+			spaceBetween: 20,
+			slidesPerView: 2
+		},
+		1200: {
+			loop: false,
+			spaceBetween: 20,
+			slidesPerColumn: 2,
+			slidesPerView: 2
+		},
+	},
+	pagination: {
+		el: '.rewiews__swiper_pagination',
+		type: 'custom',
+		renderCustom: function (swiper, current, total) {
+			return ' ещё ' + (total - current);
+		}
+	}
+
+})
+
+
+// add div .swiper-slide carousel_reviews
+function addSlide() {
+	if (window.innerWidth > 1199) {
+		var el = document.querySelector('.reviews__swiper');
+		var elem = el.querySelectorAll('.swiper-slide');
+
+		if (elem.length % 2 == 0) {
+			var wrap = document.querySelector('.reviews__swiper_wrapper');
+			wrap.insertAdjacentHTML('beforeEnd', '<div class="swiper-slide"></div>');
+			mySwiper_reviews.update();
+		}
+	}
+}
+addSlide();
+
+
+/* reviews jQery */
+
+function showText1() {
+	$('[data-id_img]').click(function () {
 		var bg = $(this).attr('src');
-		var text = $(this).attr('data-img');
-    $('[data-bg]').css('backgroundImage', 'url('+ bg +')');
+		var text = $(this).attr('data-id_img');
+		$('[data-bg]').css('backgroundImage', 'url(' + bg + ')');
+
 		$('[data-text]').css("display", "none");
-		$('[data-text='+ text +']').css("display", "block");
-  });	*/
-  
-	/*reviews js*/
+		$('[data-text=' + text + ']').css("display", "block");
+	});
+}
+showText1();
+
+/* reviews js */
+
 function showText() {
 	var elems = document.querySelectorAll('[data-id_img]');
 	for (var i = 0; i < elems.length; i++) {
@@ -139,71 +160,51 @@ function showText() {
 		}
 	}
 }
-showText();
-	
+// showText();
 
 
-	$('[data-href=teach]').click(function(){ //таже копка
-		if ($(this).hasClass('show')) {
-			$(this).removeClass('show');  
-			
-			$('.teach__item').removeClass('show2');
-			setTimeout(function(){
-				$('#teach').removeClass('show1');
-				$('.teach__item').removeClass('display');},1000);
-		} else {                                 //новая
-			var idItem = $(this).attr('data-id');
-			if ($('[data-href=teach]').hasClass('show')) { //есть активные
-				$('[data-href=teach]').removeClass('show');
+
+
+function showTeach() {
+
+	var btn = $('[data-id]');
+	var teach = $('[data-teach]');
+	var field = $('#teach');
+
+	// var heigh = $('[data-teach=' + idItem + ']').height();
+
+	var btn = $('[data-id]');
+	var teach = $('[data-teach]');
+	var field = $('#teach');
+
+	btn.click(function () {
+		var Top = field.offset().top; //плавная прокрутка к метке 
+		$('html, body').animate({
+			scrollTop: Top - 80
+		}, 500);
+
+		if ($(this).hasClass('show')) { //таже копка
+			$(this).removeClass('show');
+			teach.slideUp(500);
+
+		} else { //не эта
+
+			var id = $(this).attr('data-id');
+			var teachShow = $('[data-teach="' + id + '"]');
+
+			if (btn.hasClass('show')) { //есть активные	
+				btn.removeClass('show');
 				$(this).addClass('show');
-				
-				$('.teach__item').removeClass('show2');
-				setTimeout(function(){
-					$('.teach__item').removeClass('display');
-					$('[data-teach="'+ idItem +'"]').addClass('display');
-				},1000);
-				setTimeout(function(){$('[data-teach="'+ idItem +'"]').addClass('show2');},1100);
-			} else {                                       //нет включеных
-				$(this).addClass('show');
 
-				$('#teach').addClass('show1');
-				$('[data-teach='+ idItem +']').addClass('display');
-				setTimeout(function(){$('[data-teach="'+ idItem +'"]').addClass('show2');},1000);
+				teach.slideUp(500);
+				teachShow.slideDown(500);
+
+			} else { //нет включеных
+
+				$(this).addClass('show');
+				teachShow.slideDown(500);
 			}
 		}
-		var Top = $('#teach').offset().top;
-		$('html, body').animate({scrollTop: Top -80}, 1000);
-  });	
-	
-	
-	
-	
-//	var heigh = $('[data-teach='+ idItem +']').height();
-	
-/*	$('[data-href=teach]').click(function(){
-		if ($(this).hasClass('show')) {
-			$(this).removeClass('show');   
-			$('.teach__item').fadeOut(1000);
-			$('#teach').delay(1000).animate({height:"0px"},1000);
-		} else {
-			var idItem = $(this).attr('data-id');
-			$('[data-href=teach]').removeClass('show');
-			$(this).addClass('show');
-			$('.teach__item').fadeOut(1000);
-					
-			$('#teach').animate({height:"250px"},1000); 
-			$('[data-teach='+ idItem +']').delay(1000).fadeIn(1000);
-					
-			var Top = $('#teach').offset().top;                    //плавная прокрутка к метке 
-						$('html, body').animate({scrollTop: Top -80}, 1000);
-		}
-	});*/
-
-	
-	
-	
-	
-	
-	
-	
-	
+	});
+}
+showTeach();
