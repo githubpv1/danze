d04c86636cf9bfa4e2ea109ec65c764f1d8ff9a9
githubpv1 <pvgit20@gmail.com>
2020-09-02 18:31:50 +0300

    es

--- js/main.js
+++ js/main.js
@@ -15,9 +15,11 @@ more();
 
 //сбрасываем :focus при клике для a и button, но оставляем с клавиатуры
 
+
 function focusLose() {
 	var isMouseDown = false;
 	var button = document.querySelectorAll('a, button');
+	var isDialog = document.querySelector('[role="dialog"]');
 
 	function func() {
 		if (isMouseDown) {
@@ -29,10 +31,18 @@ function focusLose() {
 		var el = button[i];
 		el.addEventListener('mousedown', function () {
 			isMouseDown = true;
+			if (isDialog) {
+				isKeyClick = false;
+			}
 		});
 		el.addEventListener('mouseup', function () {
 			isMouseDown = false;
 		});
+		if (isDialog) {
+			el.addEventListener('keydown', function () {
+				isKeyClick = true;
+			});
+		}
 		el.addEventListener('focus', func.bind(el));
 	}
 }
